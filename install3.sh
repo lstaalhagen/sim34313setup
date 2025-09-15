6e8515c9056bb38d794182d4135bcef951479811
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-15 20:36:13 +0200

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -5,17 +5,17 @@
 REALUSER=${SUDO_USER}
 [ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
-sudo -u ${REALUSER} install --mode=0777 start_omnetpp.sh /home/user/omnetpp-6.2.0
+install -o user -g user --mode=0755 start_omnetpp.sh /home/user/omnetpp-6.2.0
 
-sudo -u ${REALUSER} rm /home/user/.local/share/applications/omnetpp*.desktop
-sudo -u ${REALUSER} install --mode=0700 OMNeTpp.desktop /home/user/.local/applications
+rm -f /home/user/.local/share/applications/omnetpp*.desktop
+install -o user -g user --mode=0700 OMNeTpp.desktop /home/user/.local/share/applications
 
 # Add user to vboxsf group
 usermod -aG vboxsf user
 
 # Handling models
 sudo -u ${REALUSER} mkdir -p /home/user/Models
-sudo -u ${REALUSER} install --mode=0755 getmodels.sh /home/user/Models
+install -o user -g user --mode=0755 getmodels.sh /home/user/Models
 install --mode=0644 getmodels.service /etc/systemd/system/getmodels.service
 systemctl daemon-reload
 systemctl enable getmodels.service
