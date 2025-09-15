07abbe4cedd3c73bed2cf128806905a2692c2978
Lars Staalhagen <lars@staalhagen.dk>
2025-09-15 15:00:51 +0200

    20250915-1500

---
+++ getmodels.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /home/user/Models
+git clone http://github.com/lstaalhagen/sim34313models
+cp sim34313models/Models*.zip .
+rm -rf sim34313models
+
--- install3.sh
+++ install3.sh
@@ -5,10 +5,21 @@
 REALUSER=${SUDO_USER}
 [ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
-install --mode=0777 start_omnetpp.sh /home/user/omnetpp-6.2.0
+sudo -u ${REALUSER} install --mode=0777 start_omnetpp.sh /home/user/omnetpp-6.2.0
 
 sudo -u ${REALUSER} rm /home/user/.local/share/applications/omnetpp*.desktop
 sudo -u ${REALUSER} install --mode=0700 OMNeTpp.desktop /home/user/.local/applications
 
 # Add user to vboxsf group
 usermod -aG vboxsf user
+
+# Handling models
+sudo -u ${REALUSER} mkdir -p /home/user/Models
+sudo -u ${REALUSER} install --mode=0755 getmodels.sh /home/user/Models
+install getmodels.service /etc/systemd/system/getmodels.service
+systemctl daemon-reload
+systemctl enable getmodels.service
+
+
+
+
