7e1aea978549b022a23d2c26663f0d755e177af0
Lars Staalhagen <lars@staalhagen.dk>
2025-09-15 13:29:41 +0200

    20250915-1330

---
+++ install3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Check for root, etc.
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
+REALUSER=${SUDO_USER}
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
+
+install --mode=0777 start_omnetpp.sh /usr/local/bin
+
+sudo -u ${REALUSER} rm /home/user/.local/share/applications/omnetpp*.desktop
+sudo -u ${REALUSER} install --mode=0700 OMNeTpp.desktop /home/user/.local/applications
---
+++ start_omnetpp.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /home/user/omnetpp-6.2.0
+source setenv 1>/dev/null 2>&1
+./bin/omnetpp 1>/dev/null 2>&1
