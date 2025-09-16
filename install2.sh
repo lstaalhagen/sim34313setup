7af41f0134b95d7e8504063d16353226b163f9c5
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-16 13:38:21 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,13 +5,5 @@
 REALUSER=${SUDO_USER}
 [ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
-# Install dependencies
-apt -y install pkg-config bison flex python3-numpy python3-scipy python3-pandas \
-               python3-matplotlib python3-ipython python3-dev qt6-base-dev qt6-base-dev-tools \
-               qmake6 libqt6svg6 qt6-wayland libwebkit2gtk-4.1-0 ccache
-
-# Install Wireshark silently
-DEBIAN_FRONTEND=noninteractive apt-get -yq install wireshark
-
 # Setup OMNeT++ as user
 sudo -u ${REALUSER} /bin/bash ./setup_omnetpp.sh
