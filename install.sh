08b1c821909c58ce10145947eb6948aadd98fef8
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-16 13:37:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,17 @@ REALUSER=${SUDO_USER}
 apt update && apt -y upgrade
 apt update && apt -y autoremove
 
+# Install dependencies
+apt -y install pkg-config bison flex python3-numpy python3-scipy python3-pandas \
+               python3-matplotlib python3-ipython python3-dev qt6-base-dev qt6-base-dev-tools \
+               qmake6 libqt6svg6 qt6-wayland libwebkit2gtk-4.1-0 ccache
+
+# Install Wireshark silently
+DEBIAN_FRONTEND=noninteractive apt-get -yq install wireshark
+
+# Clean up apt cache to save space
+apt clean
+
 # Fix Grub to have a visible menu shortly
 sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
