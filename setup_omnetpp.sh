b19a0110d71841474884e8d443386a59ca159995
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-16 12:50:49 +0200

    Create setup_omnetpp.sh

---
+++ setup_omnetpp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd /home/user
+
+# Download OMNeT++ if tgz-file is not already present
+TGZFILE=omnetpp-6.2.0-linux-x86_64.tgz
+if [ ! -f "${TGZFILE}" ] ; then
+  wget https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.2.0/${TGZFILE}
+fi
+
+# Delete old omnet directory and unpack tgz file
+if [ -d omnetpp-6.2.0 ] ; then
+  rm -rf omnetpp-6.2.0
+fi
+tar xzf $TGZFILE
+
+cd omnetpp-6.2.0
+sed -i 's/WITH_OSG=.*/WITH_OSG=no/g' configure.user
+source setenv
+./configure
+
+echo "Run 'make' command? (Will take a long time)"
+read -r ANSWER
+if [ "${ANSWER}" = "y" ] || [ "${ANSWER}" = "Y" ] ; then
+  make
+fi
