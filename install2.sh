29b7b4b62b8085797ff01f1a782173d97f9b72a2
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-16 13:46:10 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,5 +5,34 @@
 REALUSER=${SUDO_USER}
 [ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
-# Setup OMNeT++ as user
+cat << EOF > ./setup_omnetpp.sh
+#!/usr/bin/env bash
+
+cd /home/user
+
+# Download OMNeT++ if tgz-file is not already present
+TGZFILE=omnetpp-6.2.0-linux-x86_64.tgz
+if [ ! -f "${TGZFILE}" ] ; then
+  wget https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.2.0/${TGZFILE}
+fi
+
+# Delete old omnet directory and unpack tgz file
+if [ -d omnetpp-6.2.0 ] ; then
+  rm -rf omnetpp-6.2.0
+fi
+tar xzf $TGZFILE
+
+cd omnetpp-6.2.0
+sed -i 's/WITH_OSG=.*/WITH_OSG=no/g' configure.user
+source setenv
+./configure
+
+echo "Run 'make' command? (Will take a long time)"
+read -r ANSWER
+if [ "${ANSWER}" = "y" ] || [ "${ANSWER}" = "Y" ] ; then
+  make
+fi
+EOF
+
 sudo -u ${REALUSER} /bin/bash ./setup_omnetpp.sh
+rm -f ./setup_omnetpp.sh
