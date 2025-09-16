53fbcca14cfea8d293c4da52c0c1832b79d278b0
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-16 12:47:38 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -20,22 +20,23 @@ pwd
 # Download OMNeT++ if tgz-file is not already present
 TGZFILE=omnetpp-6.2.0-linux-x86_64.tgz
 if [ ! -f "${TGZFILE}" ] ; then
-  wget https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.2.0/${TGZFILE}
+  sudo -u ${REALUSER} wget https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.2.0/${TGZFILE}
+  # chown user: $TGZFILE
 fi
 
 # Delete old omnet directory and unpack tgz file
 if [ -d omnetpp-6.2.0 ] ; then
   rm -rf omnetpp-6.2.0
 fi
-tar xzf $TGZFILE
+sudo -u ${REALUSER} tar xzf $TGZFILE
 
 cd omnetpp-6.2.0
-sed -i 's/WITH_OSG=.*/WITH_OSG=no/g' configure.user
-source setenv
-./configure
+sudo -u ${REALUSER} sed -i 's/WITH_OSG=.*/WITH_OSG=no/g' configure.user
+sudo -u ${REALUSER} source setenv
+sudo -u ${REALUSER} ./configure
 
 echo "Run 'make' command? (Will take a long time)"
 read -r ANSWER
 if [ "${ANSWER}" = "y" ] || [ "${ANSWER}" = "Y" ] ; then
-  make
+  sudo -u ${REALUSER} make
 fi
