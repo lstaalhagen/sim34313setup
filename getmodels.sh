ed68052447a072f2a4887879df7cda5d4483e529
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-17 12:54:19 +0200

    Update getmodels.sh

--- getmodels.sh
+++ getmodels.sh
@@ -10,7 +10,12 @@ DIRECTORY=$(basename ${REPOSITORY})
 cd /home/user/Models
 
 # Check for repo existence
-[ "$(curl -s -o /dev/null -I -w \"%{http_code}\" ${REPOSITORY})" != "200" ] && echo "Error checking repository. Exiting" && exit 1
+GIT_TERMINAL_PROMPT=no
+${GITCMD} -ls-remote ${REPOSITORY} HEAD &>/dev/null
+if [ $? -ne 0 ] ; then
+  echo "Error checking repository. Exiting"
+  exit 1
+fi
 
 # Clone
 rm -rf ${DIRECTORY}
