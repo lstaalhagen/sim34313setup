95dc948cea8cbacfd4b2a464eed42c3418f872a7
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-09-17 12:00:01 +0200

    Update getmodels.sh

--- getmodels.sh
+++ getmodels.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-cd /home/user/Models
-/usr/bin/git clone http://github.com/lstaalhagen/sim34313models
-if [ -d sim34313models ] ; then
-  cp sim34313models/Models*.zip .
-  rm -rf sim34313models
-fi
+GITCMD="/usr/bin/git"
+REPOSITORY="https://github.com/lstaalhagen/sim34313models"
+DIRECTORY=$(basename ${REPOSITORY})
 
+# Check for git command
+[ ! -x ${GITCMD} ] && echo "Git not found. Exiting" && exit 1
+
+cd /home/user/Models
+
+# Check for repo existence
+[ "$(curl -s -o /dev/null -I -w \"%{http_code}\" ${REPOSITORY})" != "200" ] && echo "Error checking repository. Exiting" && exit 1
+
+# Clone
+rm -rf ${DIRECTORY}
+${GITCMD} clone ${REPOSITORY}
+if [ -d ${DIRECTORY} ] ; then
+  cp ${DIRECTORY}/Models*.zip .
+  rm -rf ${DIRECTORY}
+fi
