d1dc17f89d5e536cf1956a4d57d376fd7d32fd6e
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-10-01 15:04:24 +0200

    Add files via upload

--- getmodels.sh
+++ getmodels.sh
@@ -3,13 +3,15 @@
 GITCMD="/usr/bin/git"
 REPOSITORY="https://github.com/lstaalhagen/sim34313models"
 DIRECTORY=$(basename ${REPOSITORY})
+SIMDIR=/home/user/Simulations
 
 # Check for git command
 [ ! -x ${GITCMD} ] && echo "Git not found. Exiting" && exit 1
 
+# Switch to main Models directory
 cd /home/user/Models
 
-# Check for repo existence
+# Check for repo availability
 GIT_TERMINAL_PROMPT=no
 ${GITCMD} ls-remote ${REPOSITORY} HEAD &>/dev/null
 if [ $? -ne 0 ] ; then
@@ -17,10 +19,18 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
-# Clone
+# Clone repo and copy zip-files to main Models directory
 rm -rf ${DIRECTORY}
 ${GITCMD} clone ${REPOSITORY}
 if [ -d ${DIRECTORY} ] ; then
-  cp ${DIRECTORY}/Models*.zip .
+  cp ${DIRECTORY}/*.zip .
   rm -rf ${DIRECTORY}
 fi
+
+# Unzip missing project zip-files to Simulation directory
+for ZIPFILE in $(ls *.zip) ; do
+  PROJNAME=$(basename ${ZIPFILE} .zip)
+  if [ ! -d ${SIMDIR}/${PROJNAME} ] ; then
+    unzip -qq -d ${SIMDIR} ${ZIPFILE}
+  fi
+done
