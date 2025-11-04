33ee43099fffb508c9104d1184456aac412b4ebf
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2025-11-04 20:37:34 +0100

    Add files via upload

---
+++ purge-runconfigs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -d .metadata ] ; then
+  echo "Doesn't look like the current directory is a workspace. Bailing out ..."
+  exit 1
+fi
+
+rm .metadata/.plugins/org.eclipse.debug.core/.launches/*.launch
